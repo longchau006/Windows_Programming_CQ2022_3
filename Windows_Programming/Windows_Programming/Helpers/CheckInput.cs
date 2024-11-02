08fb547be0fc5c58f4f4b48e19a79b456dbea8b7
nguyentanhoangsa <nguyentanhoangsa@gmail.com>
2024-11-02 23:26:46 +0700

    Fix bug CheckInput.cs

--- Windows_Programming/Windows_Programming/Helpers/CheckInput.cs
+++ Windows_Programming/Windows_Programming/Helpers/CheckInput.cs
@@ -2,21 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Windows_Programming.Model;
 
 namespace Windows_Programming.Helpers
 {
-    internal class CheckInput
+    public class CheckInput
     {
-       static bool CheckFormatEmail(String emailInput)
+       public static bool CheckFormatEmail(string emailInput)
         {
             string emailPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
             Regex regex = new Regex(emailPattern);
             return regex.IsMatch(emailInput);
         }
 
-        static bool CheckFormatPassword(String passwordInput)
+        public static bool CheckFormatPassword(string passwordInput)
         {
             // Password must have at least 8 characters, 1 uppercase, 1 lowercase, 1 number, 1 special character
             string passwordPattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$";
