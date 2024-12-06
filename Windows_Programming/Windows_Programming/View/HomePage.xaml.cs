9105ca36ff4dc71aea7e9b155b0a602235424228
nguyentanhoangsa <118616835+PhanKai@users.noreply.github.com>
2024-12-06 12:56:43 +0700

    Update Filter in Home

--- Windows_Programming/Windows_Programming/View/HomePage.xaml.cs
+++ Windows_Programming/Windows_Programming/View/HomePage.xaml.cs
@@ -13,6 +13,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Gaming.Input.ForceFeedback;
+using Windows.System;
 using Windows_Programming.Database;
 using Windows_Programming.Model;
 using Windows_Programming.ViewModel;
@@ -32,6 +34,8 @@ namespace Windows_Programming.View
         public PlansInTrashCanViewModel MyPlansInTrashCanViewModel => MainWindow.MyPlansTrashCanViewModel;
 
         int accountId = MainWindow.MyAccount.Id;
+
+        bool check1 = false, check2 = false, check3 = false, check4 = false, check5 = false;
         public HomePage()
         {
             this.InitializeComponent();
@@ -137,5 +141,242 @@ namespace Windows_Programming.View
             }
         }
 
+        private void OnNavigationCheckBoxChecked(object sender, RoutedEventArgs e)
+        {
+            var selectedCheckBox = sender as CheckBox;
+
+            if (selectedCheckBox == Traveller_CheckBox)
+            {
+                NonTraveller_CheckBox.IsChecked = false;
+                check1 = true;
+                check2 = false;
+            }
+            else if (selectedCheckBox == NonTraveller_CheckBox)
+            {
+                Traveller_CheckBox.IsChecked = false;
+                check2 = true;
+                check1 = false;
+            }
+            else if (selectedCheckBox == UpcomingTrips_CheckBox)
+            {
+                PastTrips_CheckBox.IsChecked = false;
+                CurrentTrips_CheckBox.IsChecked = false;
+                check3 = true;
+                check4 = false;
+                check5 = false;
+               
+            }
+            else if (selectedCheckBox == PastTrips_CheckBox)
+            {
+                UpcomingTrips_CheckBox.IsChecked = false;
+                CurrentTrips_CheckBox.IsChecked = false;
+                check4 = true;
+                check3 = false;
+                check5 = false;
+            }
+            else if (selectedCheckBox == CurrentTrips_CheckBox)
+            {
+                UpcomingTrips_CheckBox.IsChecked = false;
+                PastTrips_CheckBox.IsChecked = false;
+                check5 = true;
+                check3 = false;
+                check4 = false;
+            }
+
+            CheckFilter();
+            
+        }
+        private void OnNavigationCheckBoxUnchecked(object sender, RoutedEventArgs e)
+        {
+            var uncheckedCheckBox = sender as CheckBox;
+
+            if (uncheckedCheckBox == Traveller_CheckBox)
+            {
+                check1 = false;
+            }
+            else if (uncheckedCheckBox == NonTraveller_CheckBox)
+            {
+                check2 = false;
+            }
+            else if (uncheckedCheckBox == UpcomingTrips_CheckBox)
+            {
+                check3 = false;
+            }
+            else if (uncheckedCheckBox == PastTrips_CheckBox)
+            {
+                check4 = false;
+            }
+            else if (uncheckedCheckBox == CurrentTrips_CheckBox)
+            {
+                check5 = false;
+            }
+
+            CheckFilter();  
+        }
+
+        public void CheckFilter()
+        {
+            DateTime nowTime = DateTime.Now;
+
+            if (!check1 && !check2 && !check3 && !check4 && !check5)
+            {
+                foreach (var plan in MyPlansHomeViewModel.PlansInHome)
+                {
+                    plan.IsVisible = true;
+                }
+                return;
+            }
+            else if (check1 && check3)
+            {
+                foreach (var plan in MyPlansHomeViewModel.PlansInHome)
+                {
+                    if (plan.Type && plan.StartDate > nowTime)
+                    {
+                        plan.IsVisible = true;
+                    }
+                    else
+                    {
+                        plan.IsVisible = false;
+                    }
+                }
+                return;
+            }
+            else if (check1 && check4)
+            {
+                foreach (var plan in MyPlansHomeViewModel.PlansInHome)
+                {
+                    if (plan.Type && plan.EndDate < nowTime)
+                    {
+                        plan.IsVisible = true;
+                    }
+                    else
+                    {
+                        plan.IsVisible = false;
+                    }
+                }
+                return;
+            }
+            else if (check1 && check5)
+            {
+                foreach (var plan in MyPlansHomeViewModel.PlansInHome)
+                {
+                    if (plan.Type && plan.EndDate > nowTime && plan.StartDate < nowTime)
+                    {
+                        plan.IsVisible = true;
+                    }
+                    else
+                    {
+                        plan.IsVisible = false;
+                    }
+                }
+                return;
+            }
+            else if (check2 && check3)
+            {
+                foreach (var plan in MyPlansHomeViewModel.PlansInHome)
+                {
+                    if (!plan.Type && plan.StartDate > nowTime)
+                    {
+                        plan.IsVisible = true;
+                    }
+                    else
+                    {
+                        plan.IsVisible = false;
+                    }
+                }
+                return;
+            }
+            else if (check2 && check4)
+            {
+                foreach (var plan in MyPlansHomeViewModel.PlansInHome)
+                {
+                    if (!plan.Type && plan.EndDate < nowTime)
+                    {
+                        plan.IsVisible = true;
+                    }
+                    else
+                    {
+                        plan.IsVisible = false;
+                    }
+                }
+                return;
+            }
+            else if (check2 && check5)
+            {
+                foreach (var plan in MyPlansHomeViewModel.PlansInHome)
+                {
+                    if (!plan.Type && plan.EndDate > nowTime && plan.StartDate < nowTime)
+                    {
+                        plan.IsVisible = true;
+                    }
+                    else
+                    {
+                        plan.IsVisible = false;
+                    }
+                }
+                return;
+            }
+            else if (check1)
+            {
+                foreach (var plan in MyPlansHomeViewModel.PlansInHome)
+                {
+                    plan.IsVisible = plan.Type;
+                }
+                return;
+            }
+            else if (check2)
+            {
+                foreach (var plan in MyPlansHomeViewModel.PlansInHome)
+                {
+                    plan.IsVisible = !plan.Type;
+                }
+                return;
+            }
+            else if (check3)
+            {
+                foreach (var plan in MyPlansHomeViewModel.PlansInHome)
+                {
+                    if (plan.StartDate > nowTime)
+                    {
+                        plan.IsVisible = true;
+                    }
+                    else
+                    {
+                        plan.IsVisible = false;
+                    }
+                }
+                return;
+            }
+            else if (check4)
+            {
+                foreach (var plan in MyPlansHomeViewModel.PlansInHome)
+                {
+                    if (plan.EndDate < nowTime)
+                    {
+                        plan.IsVisible = true;
+                    }
+                    else
+                    {
+                        plan.IsVisible = false;
+                    }
+                }
+                return;
+            }
+            else if (check5)
+            {
+                foreach (var plan in MyPlansHomeViewModel.PlansInHome)
+                {
+                    if (plan.EndDate > nowTime && plan.StartDate < nowTime)
+                    {
+                        plan.IsVisible = true;
+                    }
+                    else
+                    {
+                        plan.IsVisible = false;
+                    }
+                }
+                return;
+            }
+        }
     }
 }
